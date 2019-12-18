2318416441cbf737ff7da4da97c3c21ced87a873
pauldaiko <30779923+pauldaiko@users.noreply.github.com>
2019-12-18 17:03:34 +0100

    finished basic functionality of burger builder app

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Auxillary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = ( props ) => (
-        <Aux>
-        <Toolbar />
-        <main className={classes.content}>
-            {props.children}
-        </main>
-        </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+state = {
+    showSideDrawer: false
+}
+
+
+sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+}
+
+sideDrawerToggleHandler = () => {
+    this.setState(( prevState ) => {
+        return  {showSideDrawer: !prevState.showSideDrawer}
+    });
+}
+
+    render () {
+        return (
+            <Aux>
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+            <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.showSideDrawer} />
+            <main className={classes.content}>
+                {this.props.children}
+            </main>
+            </Aux>
+        )
+    }
+} 
+export default Layout;
