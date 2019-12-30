562602d3bf2e468737051b23910ebb88e76c1e98
pauldaiko <30779923+pauldaiko@users.noreply.github.com>
2019-12-30 16:26:46 +0100

    work on the contact details form

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -38,7 +38,7 @@ class Checkout extends Component {
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler} />
                 <Route path={this.props.match.path + '/contact-data'}
-                 render={(props) => (<ContactData ingredients={this.state.ingredients} proce={this.state.totalPrice} {...props} />)} />
+                 render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
             </div>
         )
     }
